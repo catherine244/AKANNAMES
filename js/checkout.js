ce027a5f35611991a3f8e90914c306f6bb25b01c
catherine244 <ccayreen24@gmail.com>
2019-10-27 15:30:31 +0300

    deleted checkout.js file

--- js/checkout.js
+++
@@ -1,8 +0,0 @@
-function checkout() {
-  var gender = document.getElementsByName("gender");
-  for (i = 0; i < gender.length; i++) {
-    if (gender[i].checked) {
-      return (gender[i].value);
-    }
-  }
-}
